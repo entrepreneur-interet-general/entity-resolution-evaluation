05978dfe09fd5ee91d7b3a1a4c62b31895ed4c9c
paulboosz <paulboosz@gmail.com>
2018-10-18 17:24:52 +0200

    added reconciliation

---
+++ entity_resolution_evaluation/reconcile.py
@@ -0,0 +1,56 @@
+from entity_resolution_evaluation.evaluation import to_set
+
+def reconcile(R,S,method):
+    """
+    If R_set, the set of elements of R and S_set are disjoints, we can't evaluate R with S.
+    First we need to reconcile R and S so that R_set and S_set are equal
+    Args:
+        R (list of list): the partition, output of the entity resolution that we want to evaluate 
+        S (list of list): the gold standard
+        method (str): the reconciliation method among 'R0S' and 'R1S1'        
+    Returns:
+        (list of list): R after reconciliation
+        (list of list): S after reconciliation
+    """
+    S_set = to_set(S)
+
+    if method == "R0S":
+        # delete all elements of R not in S        
+        R0 = []
+        for i, entity in enumerate(R):
+            R0_entity = []
+            for r, rec in enumerate(entity):
+                if rec in S_set:
+                    R0_entity.append(rec)
+            R0.append(R0_entity)
+        return R0,S
+    
+    if method == "R1S1":
+        # first let's build R1
+        R1 = []        
+        for i, entity in enumerate(R):
+            entity_partially_in_S = 0
+            for r,rec in enumerate(entity):
+                if rec in S_set:
+                    entity_partially_in_S = 1
+                    break
+            # we only add to R1, entities that have at least 1 element in S
+            if entity_partially_in_S == 1:                
+                R1.append(entity)
+        # now S1
+        S1 = S[:]
+        for i, entity in enumerate(R1):
+            new_entity = []
+            for r, rec in enumerate(entity):
+                if rec not in S_set:
+                    new_entity.append(rec)
+            S1.append(new_entity)
+
+        return R1,S1
+
+
+
+
+
+
+
--- entity_resolution_evaluation/test.py
+++ entity_resolution_evaluation/test.py
@@ -1,4 +1,5 @@
 from entity_resolution_evaluation.evaluation import gmd, evaluate, check_r_s_same_size
+from entity_resolution_evaluation.reconcile import reconcile
 import pytest
 
 
@@ -16,29 +17,30 @@ def init_data():
     S_splitted = [[0], [1], [2], [3], [4], [5]]
     S_merged = [[0, 1, 2, 3, 4, 5]]
     R4 = [[0, 1, 2, 3, 4, 6, 7]]
+    R5 = [[0,1,2,3,4,8],[5,1,10,11],[13,14]]
 
-    return S, S3, R3, S2, R, S_splitted, S_merged, R4
+    return S, S3, R3, S2, R, S_splitted, S_merged, R4, R5
 
 
 def test_gmd(init_data):
     """
     make sure the Generalized Merged Distance is computed correctly
     """
-    S, S3, R3, S2, R, S_splitted, S_merged, R4 = init_data
+    S, S3, R3, S2, R, S_splitted, S_merged, R4, R5 = init_data
     assert gmd(S, S, f1, f1) == 0
     assert gmd(S3, S, f1, f1) == 1
     assert gmd(S_splitted, S, f1, f1) == 3
 
 
 def test_evaluate_bmd(init_data):
-    S, S3, R3, S2, R, S_splitted, S_merged, R4 = init_data
+    S, S3, R3, S2, R, S_splitted, S_merged, R4, R5 = init_data
 
     assert evaluate(S, S3, 'bmd') == 1
     assert evaluate(S, S, 'bmd') == 0
 
 
 def test_evaluate_variation_of_information(init_data):
-    S, S3, R3, S2, R, S_splitted, S_merged, R4 = init_data
+    S, S3, R3, S2, R, S_splitted, S_merged, R4, R5 = init_data
     assert evaluate(R, S, 'variation_of_information') == 0.6365141682948129
     assert evaluate(S_merged, S, 'variation_of_information') == 1.0114042647073518
     assert evaluate(S_splitted, S, 'variation_of_information') == 0.7803552045207032
@@ -46,25 +48,30 @@ def test_evaluate_variation_of_information(init_data):
 
 
 def test_evaluate_precision(init_data):
-    S, S3, R3, S2, R, S_splitted, S_merged, R4 = init_data
+    S, S3, R3, S2, R, S_splitted, S_merged, R4, R5 = init_data
     assert evaluate(S_merged, S, 'precision') == 0.2666666666666667
     assert evaluate(S_splitted, S, 'precision') == 0
 
 
 def test_evaluate_recall(init_data):
-    S, S3, R3, S2, R, S_splitted, S_merged, R4 = init_data
+    S, S3, R3, S2, R, S_splitted, S_merged, R4, R5 = init_data
     assert evaluate(S_merged, S, 'recall') == 1.0
     assert evaluate(S_splitted, S, 'recall') == 0
     assert evaluate(R, S, 'recall') == 0.5
 
 def test_evaluate_f1(init_data):
-    S, S3, R3, S2, R, S_splitted, S_merged, R4 = init_data
+    S, S3, R3, S2, R, S_splitted, S_merged, R4, R5 = init_data
     assert evaluate(R, S, 'f1') == 0.5
     assert evaluate(S_merged, S, 'f1') == 0.42105263157894746
 
 
 def test_check_r_s_same_size_raises_value_error(init_data):
-    S, S3, R3, S2, R, S_splitted, S_merged, R4 = init_data
+    S, S3, R3, S2, R, S_splitted, S_merged, R4, R5 = init_data
     with pytest.raises(ValueError):
         check_r_s_same_size(R4, S)
+
+def test_reconcile_r_s(init_data):
+    S, S3, R3, S2, R, S_splitted, S_merged, R4, R5 = init_data
+    R1,S1 =  reconcile(R5,S, 'R1S1')
+    assert S1 == [[0, 1], [2, 3, 4], [5], [8], [10, 11]]  
 
